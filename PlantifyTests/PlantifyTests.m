e2d68420314dbae60dc57b27d4d0d2913cfe015b
Boris Kachscovsky <kachscovsky@gmail.com>
2014-03-08 15:11:10 +0100

    Initial Commit

---
+++ PlantifyTests/PlantifyTests.m
@@ -0,0 +1,34 @@
+//
+//  PlantifyTests.m
+//  PlantifyTests
+//
+//  Created by Boris Kachscovsky on 3/8/14.
+//  Copyright (c) 2014 Boris Kachscovsky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PlantifyTests : XCTestCase
+
+@end
+
+@implementation PlantifyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
